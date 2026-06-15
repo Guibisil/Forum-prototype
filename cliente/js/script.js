07951bdf5a419a4134bf13a9466c5ccfbdc0f282
Guibisil <guibianchisilva@gmail.com>
2026-06-15 16:54:15 -0300

    implementação da pesquisa de posts

--- cliente/js/script.js
+++ cliente/js/script.js
@@ -1,4 +1,7 @@
 const btn_login = document.getElementById("btn_login");
+const area_posts = document.getElementById('area_posts');
+const pesquisa = document.getElementById('busca_post');
+const btn_busca = document.getElementById('btn_busca'); 
 
 function carregar() {
 
@@ -11,32 +14,14 @@ function carregar() {
         menu_tema.style.right = '60px';
     } 
 
-    posts();
+    get_posts().then(dados => {
+        if (dados) {
+            criacao_post(dados);
+        }
+    });
 }
 
-async function posts() {
-    try {
-        const resposta = await fetch('http://127.0.0.1:5000/dados-posts');
-        const dados = await resposta.json();
-
-        dados.map((post) => {
-            let novo_post = `<article id="post_${post.id}" class="container my-3 p-3 borda" style="border: solid 1px; border-radius: 10px;">
-                <div>
-                    <p><b class="titulo">${post.titulo}</b><i style="font-size:smaller"> - ${post.hora}</i></p>
-                </div>
-                <div>
-                    <p class="quebra">${post.conteudo}</p>
-                </div>
-            </article>`;
-
-            area_posts.insertAdjacentHTML('afterbegin', novo_post);
-        });
-    } catch (erro) {
-        alert("Erro ao buscar dados:", erro);
-    }
-}
-
-
+//direcionameto para posts
 area_posts.addEventListener('click', (e) => {
     
     if (e.target.classList.contains('titulo')) {
@@ -49,3 +34,79 @@ area_posts.addEventListener('click', (e) => {
         }
     }
 });
+
+async function get_posts() {
+    try {
+        const resposta = await fetch('http://127.0.0.1:5000/dados-posts');
+        const dados = await resposta.json();
+
+        return dados;
+    } catch (erro) {
+        alert("Erro ao buscar dados:", erro);
+    }
+}
+
+//criação dos posts
+function criacao_post(dados) {
+    area_posts.innerHTML = '';
+
+    dados.forEach((post) => {
+        let novo_post = `<article id="post_${post.id}" class="container my-3 p-3 borda post" style="border: solid 1px; border-radius: 10px;">
+            <div>
+                <p><b class="titulo">${post.titulo}</b><i style="font-size:smaller"> - ${post.hora}</i></p>
+            </div>
+            <div>
+                <p class="quebra">${post.conteudo}</p>
+            </div>
+        </article>`;
+
+        area_posts.insertAdjacentHTML('afterbegin', novo_post);
+    });
+}
+
+
+//Busca
+pesquisa.addEventListener('input', (e) => {
+    if (pesquisa.value == null || !pesquisa.value) {
+        btn_busca.disabled = true;
+        
+        get_posts().then(dados => {
+            if (dados) {
+                criacao_post(dados);
+            }
+        });
+    } else {
+        btn_busca.disabled = false;
+    }
+});
+
+btn_busca.addEventListener('click', (e) => {
+    e.preventDefault();
+    busca();
+});
+
+function busca() {
+    const busca = pesquisa.value.toLowerCase().trim();
+    
+    get_posts().then(dados => {
+        if (dados) {
+            const compativeis = verifica_busca(dados, busca);
+            criacao_post(compativeis);
+        }
+    });
+}
+
+function verifica_busca(t, b) {
+
+    let titulos_ok = []; 
+    
+    t.forEach((post) => {
+        const post_t = post.titulo.toLowerCase();
+
+        if (post_t.includes(b)) {
+            titulos_ok.push(post); 
+        }
+    });
+
+    return titulos_ok;
+}
